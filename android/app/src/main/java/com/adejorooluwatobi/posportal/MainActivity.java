083a6e780671ca8258fdfbe99829118d73a5d23e
Adejorooluwatobi <adejorotgold1@gmail.com>
2026-06-21 17:49:51 +0100

    chore: remove android, dist-desktop, and build files from git tracking

--- android/app/src/main/java/com/adejorooluwatobi/posportal/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.adejorooluwatobi.posportal;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron');
-
-// Expose protected APIs to the renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  isElectron: () => true,
-});
